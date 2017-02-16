a6d50c585938ec62bda7a2832157b00b5e60816f
collinl34 <collinlshelby@gmail.com>
2017-02-15 21:44:18 -0800

    cdn links add to index file along with main page setup ready for angular

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const morgan = require('morgan');
 
 const db = require('./config/db.js');
 
@@ -10,11 +11,12 @@ let port = process.env.PORT || 8080;
 
 // mongoose.connect();
 
-app.use(bodyParser.json());
-app.use(express.static(__dirname + './public'));
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 
 require('./app/routes')(app);
 
