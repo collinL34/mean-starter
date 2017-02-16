406e54f03fcb886ee353938e7af2d70878b1f6ba
collinl34 <collinlshelby@gmail.com>
2017-02-16 11:47:56 -0800

    approutes add to angular frontend

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,1 +1,15 @@
 angular.module('angRoutes', [])
+    .config(['$routeProvider', '$locationProvider', function($urlProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'mainCtrl.html'
+            }),
+
+            .when('/foods', {
+                templateUrl: 'views/foods.html',
+                controller: 'FoodCtrl.html'
+            })
+
+        $locationProvider.html5Mode(true);
+    }]);
